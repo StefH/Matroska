baa8a52cd2e668f129ca1d93784eea660e6e79c1
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-01-05 21:36:14 +0100

    Readme

--- README.md
+++ README.md
@@ -20,9 +20,12 @@ A Matroska demuxer to extract Ogg Opus audio from a .webm file
 [![NuGet](https://buildstats.info/nuget/Matroska)](https://www.nuget.org/packages/Matroska)
 
 ### Usage
-Todo
+Extract
 ``` c#
-...
+var inputStream = new FileStream("test.webm", FileMode.Open, FileAccess.Read);
+var outputStream = File.OpenWrite("test.opus");
+
+MatroskaDemuxer.ExtractOggOpusAudio(inputStream, outputStream);
 ```
 
 # Credits
--- src/Matroska.Muxer/MatroskaDemuxer.cs
+++ src/Matroska.Muxer/MatroskaDemuxer.cs
@@ -6,6 +6,12 @@ namespace Matroska.Muxer
 {
     public static class MatroskaDemuxer
     {
+        public static void ExtractOggOpusAudio(Stream inputStream, Stream outputStream)
+        {
+            var doc = MatroskaSerializer.Deserialize(inputStream);
+            new OggOpusAudioStreamDemuxer(doc).CopyTo(outputStream);
+        }
+
         public static void ExtractOggOpusAudio(MatroskaDocument doc, Stream outputStream)
         {
             new OggOpusAudioStreamDemuxer(doc).CopyTo(outputStream);
