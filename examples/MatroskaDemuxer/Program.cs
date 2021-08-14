e34e48e12adc4e74835647e80059e9835966d897
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-08-14 13:26:23 +0200

    MatroskaDemuxerConsoleApp

---
+++ examples/MatroskaDemuxer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Matroska.Muxer;
+
+namespace MatroskaDemuxerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = $"C:\\Users\\{Environment.UserName}\\Downloads\\Nuclear";
+
+            foreach (var file in Directory.GetFiles(folder).Where(f => f.EndsWith(".webm")))
+            {
+                Console.WriteLine(file);
+                var outputStream = File.OpenWrite(file.Replace(".webm", ".opus"));
+                MatroskaDemuxer.ExtractOggOpusAudio(File.OpenRead(file), outputStream);
+
+                outputStream.Close();
+            }
+        }
+    }
+}
